faae14e563b0d4ed0dbee20beb8dcfeb93bda946
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-03 10:52:14 -0500

    update documentation

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -2,8 +2,26 @@ import os
 import subprocess
 
 # Set the package name, subdirectory, and output directory
-subdir = '..\w4h'
-output_dir = '.'
+subdir = '.\w4h'
+output_dir = 'docs'
+
+os.environ['PYTHONPATH'] = '..' + os.pathsep + os.environ.get('PYTHONPATH', '')
 
 # Run the pdoc command
-subprocess.run(['pdoc', subdir, '-o', output_dir])
+subprocess.run(['pdoc', '--html', subdir, '-o', output_dir, '--force'])
+
+
+#This script generates simple documentation using the pdoc3 library that can easily be published on github pages
+#import os
+#import subprocess
+
+# Set the package name, subdirectory, and output directory
+#mod = 'invplot.py'
+#output_dir = 'docs'
+
+#os.environ['PYTHONPATH'] = '..' + os.pathsep + os.environ.get('PYTHONPATH', '')
+
+# Run the pdoc command
+#subprocess.run(['pdoc3', '--html', mod, '-o', output_dir, '--force'])
+#os.chdir('./docs')
+#os.rename(mod[:-3]+'.html', 'index.html')
