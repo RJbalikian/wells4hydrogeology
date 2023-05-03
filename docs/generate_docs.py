3be7db2cab46904cf76ea94bd6e4eca0069ddaf0
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-03 11:11:08 -0500

    update docs structure

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import pathlib 
 
 # Set the package name, subdirectory, and output directory
 subdir = '.\w4h'
@@ -10,6 +11,25 @@ os.environ['PYTHONPATH'] = '..' + os.pathsep + os.environ.get('PYTHONPATH', '')
 # Run the pdoc command
 subprocess.run(['pdoc', '--html', subdir, '-o', output_dir, '--force'])
 
+workDir = pathlib.Path(os.getcwd())
+if workDir.stem == 'docs':
+    pass
+elif 'docs' in str(workDir):
+    pass
+else:
+    for p in pathlib.Path('.').rglob('*'): 
+        if 'docs\\' in str(p):
+            docsPath = pathlib.Path(str(p)[:str(p).find('docs')+ 4])
+            os.chdir(docsPath)
+            break
+
+src_path = pathlib.Path('./w4h')
+trg_path = src_path.parent # gets the parent of the folder 
+
+for each_file in src_path.glob('*.*'): # grabs all files
+    each_file.rename(trg_path.joinpath(each_file.name)) # moves to parent folder.
+
+os.rmdir('./w4h')
 
 #This script generates simple documentation using the pdoc3 library that can easily be published on github pages
 #import os
