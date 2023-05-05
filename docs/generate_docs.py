6e7589e74502ee67e5d0ea7402401f4a8686aedd
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-05 09:17:10 -0500

    docs updates

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,6 +1,7 @@
 import os
-import subprocess
 import pathlib 
+import shutil
+import subprocess
 
 # Set the package name, subdirectory, and output directory
 subdir = '.\w4h'
@@ -30,5 +31,17 @@ for each_file in src_path.glob('*.*'): # grabs all files
     destFilePath = trg_path.joinpath(each_file.name)
     if destFilePath.is_file():
         os.remove(destFilePath)
-    each_file.rename(destFilePath) # moves to parent folder.
+    each_file = each_file.rename(destFilePath) # moves to parent folder.
+    if each_file.name == 'index.html':
+        mainhtmlFPath = each_file.parent.parent.joinpath('main.html')
+        if mainhtmlFPath.is_file():
+            os.remove(mainhtmlFPath)
+        each_file.rename(mainhtmlFPath)
 os.rmdir('./w4h')
+
+#Copy main readme file into docs so github pages will read it
+repo_path = pathlib.Path('..')
+
+for each_file in repo_path.iterdir():
+    if each_file.name == 'README.md':
+        shutil.copy(src=str(each_file), dst='.')
