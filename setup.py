903ddb1b6fece213e33c1528779ef2519a59da7c
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-07-19 16:54:27 -0500

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="wells4hydrogeology",
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
-    version="0.0.3",
+    version="0.0.4",
     install_requires=["geopandas", "rioxarray", "owslib", "scipy", "matplotlib", "pandas", "numpy"],
     long_description = long_description,
     long_description_content_type="text/markdown",
