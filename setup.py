0e0a3d97f02f3b5211500424486732cf53ef18b7
Riley <rileybalikian@outlook.com>
2023-12-08 12:46:57 -0600

    update deployments

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
@@ -9,9 +9,10 @@ setup(
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
     version="0.0.16",
+    package_data={'w4h': ['resources/*', 'resources/sample_data/*', 
+                          'resources/sample_data/DictionaryTerms/*','resources/sample_data/LithologyInterpretations/*']},
     install_requires=["geopandas", "rioxarray", "owslib", "scipy", "matplotlib", "pandas", "numpy"],
-    long_description = long_description,
     long_description_content_type="text/markdown",
-    description="A package to read in geology data from wells and create a layered, gridded hydrogeologic model of a study region, all within a python environment, automating and performing tasks often carried out in a dedicated GIS software.",
-    package_data={'w4h': ['resources/*', 'resources/sample_data/*', 'resources/sample_data/DictionaryTerms/*','resources/sample_data/LithologyInterpretations/*']}
+    long_description = long_description,
+    packages=find_packages(),
     )
