e5ec0b9357ab70ddace18fc887fd2d47dc359584
Riley <rileybalikian@outlook.com>
2024-03-04 11:38:02 -0600

    minor formatting updates

--- w4h/__init__.py
+++ w4h/__init__.py
@@ -1,47 +1,50 @@
 #__init__.py
+"""
+   This is the wells4hydrogeology package. 
+   
+   It contains the functions needed to convert raw well descriptions into usable (hydro)geologic data.
 
-
-#from w4h import classify, clean, export, layers, mapping, read
+"""
 
 from w4h.core import(logger_function,
                         verbose_print,
                         run,
                         get_resources)
 
-from w4h.classify import (specific_define, 
-                          split_defined, 
+from w4h.classify import (specific_define,
+                          split_defined,
                           start_define,
                           wildcard_define,
-                          remerge_data, 
-                          depth_define, 
-                          export_undefined, 
-                          fill_unclassified, 
-                          merge_lithologies, 
+                          remerge_data,
+                          depth_define,
+                          export_undefined,
+                          fill_unclassified,
+                          merge_lithologies,
                           get_unique_wells,
                           sort_dataframe)
 
-from w4h.clean import (remove_nonlocated, 
-                       remove_no_topo, 
-                       remove_no_depth, 
-                       remove_bad_depth, 
+from w4h.clean import (remove_nonlocated,
+                       remove_no_topo,
+                       remove_no_depth,
+                       remove_bad_depth,
                        remove_no_description)
 
 from w4h.export import (export_dataframe,
                         export_grids)
 
 from w4h.layers import (get_layer_depths,
-                        merge_metadata, 
-                        layer_target_thick, 
+                        merge_metadata,
+                        layer_target_thick,
                         layer_interp,
                         combine_dataset)
 
-from w4h.mapping import (read_study_area, 
-                         coords2geometry, 
-                         clip_gdf2study_area, 
-                         sample_raster_points, 
-                         xyz_metadata_merge, 
+from w4h.mapping import (read_study_area,
+                         coords2geometry,
+                         clip_gdf2study_area,
+                         sample_raster_points,
+                         xyz_metadata_merge,
                          read_wms,
-                         read_wcs, 
+                         read_wcs,
                          grid2study_area,
                          read_model_grid,
                          read_grid,
