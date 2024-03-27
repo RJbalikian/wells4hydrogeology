cffd4e7ce60c41cc42be03771fc48b825d152570
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-03-27 12:18:06 -0500

    add readthedocs links for pypi, minor formatting updates to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from setuptools import setup, find_packages
+"""Setup project"""
 
 from pathlib import Path
+from setuptools import setup, find_packages
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="wells4hydrogeology",
-    author= "Riley Balikian",
-    author_email = "balikian@illinois.edu",
-    version="0.0.22-dev",
-    package_data={'w4h': ['resources/*', 'resources/sample_data/*', 'resources/sample_data/statewide_sample_data/*',
-                          'resources/sample_data/DictionaryTerms/*','resources/sample_data/LithologyInterpretations/*']},
-    install_requires=["geopandas", "rioxarray", "owslib", "scipy", "matplotlib", "pandas", "numpy"],
-    long_description_content_type="text/markdown",
-    long_description = long_description,
-    packages=find_packages(),
-    )
+        name="wells4hydrogeology",
+        author="Riley Balikian",
+        author_email="balikian@illinois.edu",
+        version="0.0.22-dev",
+        package_data={'w4h': ['resources/*', 'resources/sample_data/*',
+                      'resources/sample_data/statewide_sample_data/*',
+                      'resources/sample_data/DictionaryTerms/*',
+                      'resources/sample_data/LithologyInterpretations/*']},
+        install_requires=["geopandas", "rioxarray", "owslib", "scipy",
+                          "matplotlib", "pandas", "numpy"],
+        long_description_content_type="text/markdown",
+        long_description=long_description,
+        packages=find_packages(),
+        )
