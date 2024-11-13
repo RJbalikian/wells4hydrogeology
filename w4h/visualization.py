d324258fb49c4b437431fc8fd12865de958c9a86
Riley <rileybalikian@outlook.com>
2024-11-13 11:37:37 -0600

    finished getting cross section data, need to plot

--- w4h/visualization.py
+++ w4h/visualization.py
@@ -55,12 +55,95 @@ def plot_profile(dataset, profile=None, profile_direction=None, xcoord='x', ycoo
         print(f"\tprofile and profile_direction must be the same length, but len(profile)={len(profile)} and len(profile_direction)={len(profile_direction)}")
         return
     
-    # Next steps:
-    # 1) For each profile/linestring, get segments
-    # 2) For each segment, generate coordinates to use
-    # 3) For each coordinate, get data for all layers
-    # 4) Create a DataArray for each segment
-    # 5) Plot all DataArrays in subplot
-    # 6) Plot map of profile(s)
-    
-    return
+
+
+    pLengths = []
+    pSegLengths = []
+    pSegOrients = []
+    profileSamplePoints = []
+    profileDicts = []
+    # Iterate through each profile
+    for p in profile:
+        # Temporary lists for each segment/vertex
+        segLengths = []
+        segOrientations = []
+        segSamplePoints = []
+
+        # Iterate through each segment/vertex of the profile
+        for i, vertex in enumerate(p.coords):
+            if i==0:
+                continue
+            prevVertex = p.coords[i-1]
+
+            segmentLineString = shapely.LineString([prevVertex, vertex])
+            segXCoords = dataset.sel(x=slice(prevVertex[0], vertex[0])).coords['x'].values
+            segYCoords = dataset.sel(x=slice(prevVertex[1], vertex[1])).coords['x'].values
+            segXCoordsDists = np.diff(segXCoords)
+            segYCoordsDists = np.diff(segYCoords)
+            segLengths.append(segmentLineString.length)
+
+            # Get distance of each segment
+            y1 = vertex[1]
+            y0 = prevVertex[1]
+            x1 = vertex[0]
+            x0 = prevVertex[0]
+            yDist = y1-y0
+            xDist = x1-x0
+
+            # Get orientation of segment
+            segOrient='WE'
+            if abs(yDist) > abs(xDist):
+                segOrient = 'SN'
+                if yDist < 0:
+                    segOrient = 'NS'
+            else:
+                if xDist < 0:
+                    segOrient = 'EW'
+            segOrientations.append(segOrient)
+            yOrients = ['NS', "SN"]
+            xOrients = ['WE', "EW"]
+
+            # Get coordinates to sample for profile
+            if segOrient in xOrients:
+                vertexIndex = 0
+                otherVertextInd = 1
+                segCoords = segXCoords
+                segCoordsDists = segXCoordsDists 
+
+            else: # if oriented more NS
+                vertexIndex = 1
+                otherVertextInd = 0
+                segCoords = segYCoords
+                segCoordsDists = segYCoordsDists 
+
+            # For each segment, get each xy value as shapely point at each existing coordinate value
+            subsegSamplePoints = []
+            for i, segmentSegment in enumerate(segCoordsDists):
+                currSubsegDistance = np.sum(segCoordsDists[:i+1])
+                subsegSamplePoints.append(segmentLineString.line_interpolate_point(distance=currSubsegDistance))
+            segSamplePoints.extend(subsegSamplePoints)
+
+        profileLineString = shapely.LineString(segSamplePoints)
+        surfaceElevs = []
+        bedrockElevs = []
+        layerElevs = []
+        modelData = []
+        for point in segSamplePoints:
+            xcoord, ycoord = point.xy
+            surfaceElevs.append(dataset['Surface_Elevation'].sel(x=xcoord, y=ycoord, method='nearest').values.flatten)
+            bedrockElevs.append(dataset['Bedrock_Elevation'].sel(x=xcoord, y=ycoord, method='nearest').values)
+            modelData.append(dataset['Model_Layers'].sel(x=xcoord, y=ycoord, method='nearest').values)
+            layerElevs.append(dataset.coords['layer_elevs'].sel(x=xcoord, y=ycoord, method='nearest').values)
+        
+        surfElevArr = np.array(surfaceElevs).flatten()
+        bedElevArr = np.array(bedrockElevs).flatten()
+
+        profileDicts.append({'Surface_Elevation': surfElevArr,
+                            'Bedrock_Elevation': bedElevArr,
+                            'Model_Layers': np.array(modelData).reshape((surfElevArr.shape[0], 9)),
+                            'layer_elevs': np.array(layerElevs).reshape((surfElevArr.shape[0], 9))
+                            })
+            
+    # Now just need to plot, got data
+
+    return profileDicts
